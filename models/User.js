e8086045d81e3f7196d5facb8a199882e082f1d1
Marco Mata <patrickstar1281@yahoo.com>
2022-08-04 22:08:56 -0500

    set up structure for users model

--- models/User.js
+++ models/User.js
@@ -37,9 +37,27 @@ User.init(
                 len: [4]
             }
         }
-
+    },
+    {
+        hooks: {
+            //set up beforeCreate lifecycle 'hook' functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            //set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         
-    }
-)
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }  
+);
 
 module.exports = User;
