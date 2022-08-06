7a496463f2f1668424fd3c765d28e2ff7feb4d80
Marco Mata <patrickstar1281@yahoo.com>
2022-08-06 12:48:58 -0500

    added pictures to charater model

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -14,9 +14,17 @@ router.post('/',(req,res) => {
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
-    }).then((dbUserData) => {res.json(dbUserData)})
+    }).then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
+          res.json(dbUserData);
+        });
+      })
     .catch((err) => {console.log(err); res.status(500).json(err)});
 
-}) 
+})
 
 module.exports = router;
